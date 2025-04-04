959bc8a132bf10a8dbc242da5c14975167f95ead
Chuong <tindien1995@gmail.com>
2025-04-04 11:53:34 +0700

    Add Spring Boot and React projects

---
+++ BE/src/main/java/com/example/stationerymgntbe/StationeryMgntBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.stationerymgntbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationeryMgntBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationeryMgntBeApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/com/example/stationerymgntbe/StationeryMgntBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stationerymgntbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StationeryMgntBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
